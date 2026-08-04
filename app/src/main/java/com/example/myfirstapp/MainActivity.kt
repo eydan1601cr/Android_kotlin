1b03474e11675cdd641271580fa83eab49afbd42
eydan1601cr <eydan1601@gmail.com>
2026-08-04 08:26:28 -0600

    Seccion 3 referencia_elements

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -13,7 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val boton = findViewById<Button>(R.id.boton)
+        //val boton = findViewById<Button>(R.id.boton)
+        val boton: Button= findViewById(R.id.boton)
 
         boton.setOnClickListener {
             Toast.makeText(this, "Esto es un toast", Toast.LENGTH_SHORT).show()
